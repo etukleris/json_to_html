c2572aaea51b612b94942036ff9c2f54c7edb39d
etukleris <49657933+etukleris@users.noreply.github.com>
2019-04-19 23:34:22 +0300

    Update json_to_html.go

--- json_to_html.go
+++ json_to_html.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"strconv"
 	"fmt"
 	"os"
 	"time"
@@ -15,7 +14,10 @@ func main() {
 
 	data, err := getFromUrl(url)
 	if err == nil {
-		writeToFile(data)
+		err = writeToFile(data)
+            if err != nil {
+                fmt.Println("There was an error: ", err)
+            }
 	}
 }
 	
@@ -39,7 +41,7 @@ func getFromUrl(url string) ([]byte, error) {
 	//program writes to current_dir\posts; currnent_dir being the directory where
 	//the program is placed
 
-func writeToFile(content []byte) {
+func writeToFile(jsonByteSlice []byte) error {
 
 	postsDir := "posts/"
 	if !fileExists(postsDir){
@@ -68,7 +70,7 @@ func writeToFile(content []byte) {
 	f, err := os.Create(outputFile)
     if err != nil {
         fmt.Println(err)
-        return
+        return err
     }
 	
 	defer f.Close()
@@ -77,64 +79,45 @@ func writeToFile(content []byte) {
     _, err = f.WriteString("<html>\n\n<body>\n\n")
     if err != nil {
         fmt.Println(err)
-        return
+        return err
     }
 
 	
 	//struct for json; further code to extract info from content variable
 	//>> shouldn't really be called person and people but oh well	
-	jsonByteSlice := content
 	type Person struct {
-			userId int
-			id  int
-			title  string
-			body  string
+			UserId int
+			Id  int
+			Title  string
+			Body  string
          }
 	var people []Person
 
-	var personMap []map[string]interface{}
-
-	err = json.Unmarshal(jsonByteSlice, &personMap)
+	err = json.Unmarshal(jsonByteSlice, &people)
 
 	if err != nil {
 		fmt.Println(err)
-		return
+		return err
 	}
 
-	for _, personData := range personMap {
-
-	// convert map to array of Person struct
-		var p Person
-		
-		p.userId, _ = strconv.Atoi(fmt.Sprintf("%v", personData["userId"]))
-		p.id, _ = strconv.Atoi(fmt.Sprintf("%v", personData["id"]))
-		p.title = fmt.Sprintf("%s", personData["title"])
-		p.body = fmt.Sprintf("%s", personData["body"])
-		people	 = append(people, p)
-
-	 }
 	 //write data to the file; using title and body as task requests
 	for _, person := range people{
 	
-		_, err = f.WriteString("<h1>" + person.title + "</h1>\n\n")
+		_, err = f.WriteString("<h1>" + person.Title + "</h1>\n\n" +
+                               "<p>" + person.Body + "</p>\n\n")
 		if err != nil {
 			fmt.Println(err)
-			return
-		} 
-
-		_, err = f.WriteString("<p>" + person.body + "</p>\n\n")
-		if err != nil {
-			fmt.Println(err)
-			return
+			return err
 		} 
 
 	}
 	    _, err = f.WriteString("</html>\n\n</body>\n\n")
     if err != nil {
         fmt.Println(err)
-        return
+        return err
     }
-
+    
+    return nil
 
 }
 
@@ -147,5 +130,3 @@ func fileExists(filename string) bool {
 	}
 	return !info.IsDir()
 }
-
-
